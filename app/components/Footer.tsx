a9b2352ae4428f3005b65daca6c928ce6242d63b
Neon <120107770+TomLayan1@users.noreply.github.com>
2025-05-11 10:47:27 +0100

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,22 @@ import React from 'react'
 const Footer:React.FC = () => {
   return (
     <section className='w-full py-14'>
-      Footer
+      <div className='container w-[95%] mx-auto'>
+        <div>
+         <div>
+          <p>Home</p>
+          <p>About</p>
+          <p>Projects</p>
+          <p>Contact/p>
+         </div>
+
+         <div>
+          <FaGitHub size={25} />
+          <FaLinkedIn size={25} />
+         </div>
+         <p>TOMISIN 2025</p>
+        </div>
+      </div>
     </section>
   )
 }
